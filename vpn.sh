132faf96654f02f767ef2c95f7b91eaad95541e5
Sergey Andriushchenko <114949437+fatalitirar1@users.noreply.github.com>
2025-05-06 18:36:19 +0300

    bash script for ovpn

---
+++ vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -n "$VPN_PATH" ] 
+then
+IFS=$'\n'
+v=0
+files=($(ls "$VPN_PATH"))
+for l in "${files[@]}" 
+do
+   echo "$v) $l"
+   ((v++))
+done 
+read -p "choose one:" index
+if [ $index -ge 0 ] && [ "$index" -lt "${#files[@]}" ]
+then
+config=$(echo ${files[$index]})
+      echo "connectiong to ('$config')"
+      sudo openvpn "$VPN_PATH/$config"
+   else 
+      echo "wrong index"
+fi
+   else 
+   echo "set VPN_PATH constant"
+fi 
